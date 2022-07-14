7ca989a9a1500ea1f5230584756d041e8ca743f1
Octavio Serrano <octavioserrano@Octavios-iMac.local>
2022-07-13 23:43:29 -0700

    shelf

---
+++ cart.py
@@ -0,0 +1,8 @@
+from items import shelf 
+
+class Cart:
+    def __init__(self, account, balance):
+        self.account = None
+        self.balance = None
+        
+    def add_to_cart(self):
---
+++ items.py
@@ -0,0 +1,27 @@
+shelf = [
+    {
+    'name': 'headsets',
+    'unit price': 1500,
+    'rating': 3,
+    'stock': 40},
+    {
+    'name': 'keyboard',
+    'unit price': 34,
+    'rating': 2,
+    'stock': 40},
+    {
+    'name': 'mouse',
+    'unit price': 23,
+    'rating': 4,
+    'stock': 40},
+    {
+    'name': 'display',
+    'unit price': 250,
+    'rating': 5,
+    'stock': 40},
+    {
+    'name': 'desk',
+    'unit price': 2500,
+    'rating': 5,
+    'stock': 40}
+]
