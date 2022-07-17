acd777632a7a76f06b0839e9080d06ae26649736
Octavio Serrano <octavioserrano@Octavios-iMac.local>
2022-07-17 16:41:14 -0700

    df

--- cart.py
+++ cart.py
@@ -1,4 +1,5 @@
 from items import shelf 
+import pandas as pd
 
 class Cart:
     def __init__(self, account, balance):
@@ -6,3 +7,9 @@ class Cart:
         self.balance = None
         
     def add_to_cart(self):
+        df = pd.DataFrame(shelf)
+        print(df)
+        selected_item_id = input('''select the id of the item you \n
+                                want to buy\n:''')
+        selected_item_quantity = input('''How many do you want\n:''')
+        print(selected_item_id, selected_item_quantity)
